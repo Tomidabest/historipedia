41bb90024890e971e1277751368476e32264f3e0
Tomi\Tomislav <rusevtomy@gmail.com>
2024-06-14 19:37:23 +0300

    Project Organization

---
+++ docker_config/init-mariadb.sql
@@ -0,0 +1,14 @@
+GRANT ALL PRIVILEGES ON *.* TO 'maxscale_user'@'%' IDENTIFIED BY 'secure_pass123';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE historipedia;
+USE historipedia;
+
+CREATE TABLE historical_entries (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    year INT NOT NULL
+);
+
+INSERT INTO historical_entries (title, description, year) VALUES ('Founding of Rome', 'Legend of Romulus and Remus', -753);
